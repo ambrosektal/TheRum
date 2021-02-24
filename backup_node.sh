30acf6faf4686a52e0a04db007dc70caa86debbe
Ambrose KTal <joe.spann@gmail.com>
2021-02-24 06:17:19 -0500

    new files

---
+++ backup_node.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+# -Acdtrux
+repo="/opt/repo/"
+directory="/etc/verdaccio/storage/"
+
+if [[ -e "/opt/repo/backup_node.tar" ]] ;
+then
+#    echo "exists"
+    mv -f $repo/backup_node.tar $repo/backup_node.tar.$(echo $(date "+%Y%m%d"))
+    # Backup all folders changed in the last X days
+    touch $directory/backup_node.tar
+
+    '/usr/bin/find' "$directory" -type f -mtime -14 -exec '/usr/bin/tar' rvf $repo/backup_node.tar {} \;  
+    
+else
+#    echo "doesn't exist"
+    # Backup all folders changed in the last X days
+    touch $directory/backup_node.tar
+    '/usr/bin/find' "$directory" -type f -mtime -14 -exec '/usr/bin/tar' rvf $repo/backup_node.tar {} \;
+fi
+
---
+++ backup_remi.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+directory="/opt/repo"
+
+if [[ -e "/opt/repo/backup_remi.tar" ]] ;
+then
+#    echo "exists"
+    mv -f $directory/backup_remi.tar $directory/backup_remi.tar.$(echo $(date "+%Y%m%d"))
+    touch $directory/backup_remi.tar
+    # Backup all folders changed in the last X days
+    for i in ` '/usr/bin/ls' $directory | '/usr/bin/grep' -v javascript | '/usr/bin/grep' -v transfer | '/usr/bin/grep' -v shares | '/usr/bin/grep' -v php_src ` ; 
+        do '/usr/bin/find' "$directory/$i" -type f -mtime -14 -exec '/usr/bin/tar' rvf $directory/backup_remi.tar {} \; ; 
+    done
+else
+#    echo "doesn't exist"
+    # Backup all folders changed in the last X days
+    touch $directory/backup_remi.tar
+    for i in ` '/usr/bin/ls' $directory | '/usr/bin/grep' -v javascript | '/usr/bin/grep' -v transfer | '/usr/bin/grep' -v shares  | '/usr/bin/grep' -v php_src ` ; 
+        do '/usr/bin/find' "$directory/$i" -type f -mtime -14 -exec '/usr/bin/tar' rvf $directory/backup_remi.tar {} \; ; 
+    done
+fi
+
