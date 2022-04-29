3f2fc16219eaa4bea7bc2a7f10a19c78ac343c9f
Ambrose KTal <joe.spann@gmail.com>
2022-04-29 10:21:39 -0400

    updated list

--- npmDownload.ps1
+++ npmDownload.ps1
@@ -6,7 +6,9 @@ $npmFiles = $top1000npmFiles
 
 # $npmFiles = "@apollo","@apollo-micro-server","@apollo-server","@aspnet","@axios","@babel","@babel/eslint","@babel/eslint-parser","@bootstrap","@canvas","@canvas-","@docker","@dotnet","@elastic","@elastic/apm","@elastic/elastic","@elastic/elastic-apm","@ember","@eslint","@express","@gatsby","@gitbeaker","@gitbeaker/node","@graphql","@graphql-tools","@grpc","@gulp","@jetbrains","@kibana","@lerna","@logstash","@mapbox","@microsoft","@mongodb","@mui","@next","@next-js","@nextjs","@npm","@npm-","@parcel","@pnpm","@quire","@razzle","@react","@rush","@searchkit","@solid","@solid.js","@threejs","@types","@types/vscode","@typescript","@verdaccio","@vscode","@vscode-logging","@vscode-logging/types","@vscode/sqlite3","@webpack","@websocket","@yarn","3d","3ds","ansi-color","apm","apollo","apollo-micro-server","apollo-server","aspnet","axios","babel","backend","bootstrap","canvas","canvas-","code-server","component","cra-template","create-backend","create-next","create-next-","create-react","create-react-","css","docker","dom","dotnet","elastic","elastic-apm","ember","ember-cli","env-editor","eslint","eslint-config","eslint-config-react","eslint-config-react-app","exporter","express","fontawesome","form","fortawesome","fs-extra","game","gatsby","gem","gitbeaker","graphql","grpc","gulp","gyp","helm","hook","jetbrains","kestrel","kube","kubernetes","lerna","materialui","mongo","mongodb","mui","next","next-connect","next-js","nextjs","node-gyp","node-pre-gyp","npm","npm-","parcel","pnpm","powershell","prom","prometheus","pty","quire","rails","rancher","razzle","react","react-","react-chart","react-chartjs","react-chartjs-2","react-form","react-hook","react-hook-form","react-use","react-virtual","react-window","ruby","rush","search","searchkit","server","skia","skiasharp","solid","solidjs","swr","template","threejs","threejs-","tty","typescript","undefsafe","unity","verdaccio","vscode","vscode-chrome-debug-core","vscode-jsonrpc","vscode-languageserver","vscode-languageserver-types","vscode-textmate","vscode-uri","vscode-xterm","webpack","websocket","windows","yarn","gridfs","@gridfs","multer","@multer"
 
-$npmFiles = "@apollo", "@apollo-micro-server", "@apollo-server", "@aspnet", "@axios", "@babel", "@babel/eslint", "@babel/eslint-parser", "@bootstrap", "@canvas", "@canvas-", "@docker", "@dotnet", "@elastic", "@elastic/apm", "@elastic/elastic", "@elastic/elastic-apm", "@ember", "@eslint", "@express", "@gatsby", "@gitbeaker", "@gitbeaker/node", "@graphql", "@graphql-tools", "@grpc", "@gulp", "@jetbrains", "@kibana", "@lerna", "@logstash", "@mapbox", "@microsoft", "@mongodb", "@mui", "@next", "@next-js", "@nextjs", "@npm", "@npm-", "@parcel", "@pnpm", "@quire", "@razzle", "@react", "@rush", "@solid", "@solid.js", "@threejs", "@types", "@types/vscode", "@typescript", "@verdaccio", "@vscode", "@vscode-logging", "@vscode-logging/types", "@vscode/sqlite3", "@webpack", "@websocket", "@yarn", "3d", "3ds", "ansi-color", "apm", "apollo", "apollo-micro-server", "apollo-server", "aspnet", "axios", "babel", "backend", "bootstrap", "canvas", "canvas-", "code-server", "component", "cra-template", "create-backend", "create-next", "create-next-", "create-react", "create-react-", "css", "docker", "dom", "dotnet", "elastic", "elastic-apm", "ember", "ember-cli", "env-editor", "eslint", "eslint-config", "eslint-config-react", "eslint-config-react-app", "exporter", "express", "fontawesome", "form", "fortawesome", "fs-extra", "game", "gatsby", "gem", "gitbeaker", "graphql", "grpc", "gulp", "gyp", "helm", "hook", "jetbrains", "kestrel", "kube", "kubernetes", "lerna", "materialui", "mongo", "mongodb", "mui", "next", "next-connect", "next-js", "nextjs", "node-gyp", "node-pre-gyp", "node-pty", "npm", "npm-", "parcel", "pnpm", "powershell", "prom", "prometheus", "pty", "quire", "rails", "rancher", "razzle", "react", "react-", "react-chart", "react-chartjs", "react-chartjs-2", "react-form", "react-hook", "react-hook-form", "react-use", "react-virtual", "react-window", "ruby", "rush", "server", "skia", "skiasharp", "solid", "solidjs", "swr", "template", "threejs", "threejs-", "tty", "typescript", "undefsafe", "unity", "verdaccio", "vscode", "vscode-chrome-debug-core", "vscode-jsonrpc", "vscode-languageserver", "vscode-languageserver-types", "vscode-textmate", "vscode-uri", "vscode-xterm", "webpack", "websocket", "windows", "yarn", "gridfs", "@gridfs", "multer", "@multer","starwars","star-wars","startrek","star-trek","starwars-cord","algorithm","algorithms","vue","@vue","vuejs","vue-cli","golang","go-lang","vuetify","buefy","bootstrap-vue","vue-material","quasar","vux","iview","vuew-material-kit","mint-ui","vue-router","create-vue","storybook","vuepress","nuxt","element-ui","vuex","vue-apollo","gridsome","vue-multiselect","filepond","vuelidate","eagle.js","weex","vue-resource","vuestic","vee-validate","poi","keen-ui","cube-ui","primevue","ant-design-vue","vue-tailwind","chakra-ui","vuesax","fish-ui","quasar-framework","@coreui/vue","@syncfusion/ej2-vue-base","@chakra-ui","@chakra-ui/vue","vue-material","naive-ui","@carbon","@carbon/ui","at-ui","@vue/cli","cjs"
+$npmFiles = "@apollo", "@apollo-micro-server", "@apollo-server", "@aspnet", "@axios", "@babel", "@babel/eslint", "@babel/eslint-parser", "@bootstrap", "@canvas", "@canvas-", "@docker", "@dotnet", "@elastic", "@elastic/apm", "@elastic/elastic", "@elastic/elastic-apm", "@ember", "@eslint", "@express", "@gatsby", "@gitbeaker", "@gitbeaker/node", "@graphql", "@graphql-tools", "@grpc", "@gulp", "@jetbrains", "@kibana", "@lerna", "@logstash", "@mapbox", "@microsoft", "@mongodb", "@mui", "@next", "@next-js", "@nextjs", "@npm", "@npm-", "@parcel", "@pnpm", "@quire", "@razzle", "@react", "@rush", "@solid", "@solid.js", "@threejs", "@types", "@types/vscode", "@typescript", "@verdaccio", "@vscode", "@vscode-logging", "@vscode-logging/types", "@vscode/sqlite3", "@webpack", "@websocket", "@yarn", "3d", "3ds", "ansi-color", "apm", "apollo", "apollo-micro-server", "apollo-server", "aspnet", "axios", "babel", "backend", "bootstrap", "canvas", "canvas-", "code-server", "component", "cra-template", "create-backend", "create-next", "create-next-", "create-react", "create-react-", "css", "docker", "dom", "dotnet", "elastic", "elastic-apm", "ember", "ember-cli", "env-editor", "eslint", "eslint-config", "eslint-config-react", "eslint-config-react-app", "exporter", "express", "fontawesome", "form", "fortawesome", "fs-extra", "game", "gatsby", "gem", "gitbeaker", "graphql", "grpc", "gulp", "gyp", "helm", "hook", "jetbrains", "kestrel", "kube", "kubernetes", "lerna", "materialui", "mongo", "mongodb", "mui", "next", "next-connect", "next-js", "nextjs", "node-gyp", "node-pre-gyp", "node-pty", "npm", "npm-", "parcel", "pnpm", "powershell", "prom", "prometheus", "pty", "quire", "rails", "rancher", "razzle", "react", "react-", "react-chart", "react-chartjs", "react-chartjs-2", "react-form", "react-hook", "react-hook-form", "react-use", "react-virtual", "react-window", "ruby", "rush", "server", "skia", "skiasharp", "solid", "solidjs", "swr", "template", "threejs", "threejs-", "tty", "typescript", "undefsafe", "unity", "verdaccio", "vscode", "vscode-chrome-debug-core", "vscode-jsonrpc", "vscode-languageserver", "vscode-languageserver-types", "vscode-textmate", "vscode-uri", "vscode-xterm", "webpack", "websocket", "windows", "yarn", "gridfs", "@gridfs", "multer", "@multer","starwars","star-wars","startrek","star-trek","starwars-cord","algorithm","algorithms","vue","@vue","vuejs","vue-cli","golang","go-lang","vuetify","buefy","bootstrap-vue","vue-material","quasar","vux","iview","vuew-material-kit","mint-ui","vue-router","create-vue","storybook","vuepress","nuxt","element-ui","vuex","vue-apollo","gridsome","vue-multiselect","filepond","vuelidate","eagle.js","weex","vue-resource","vuestic","vee-validate","poi","keen-ui","cube-ui","primevue","ant-design-vue","vue-tailwind","chakra-ui","vuesax","fish-ui","quasar-framework","@coreui/vue","@syncfusion/ej2-vue-base","@chakra-ui","@chakra-ui/vue","vue-material","naive-ui","@carbon","@carbon/ui","at-ui","@vue/cli","cjs","blockly","@blockly","socketio","socket.io","@socketio","@socket.io","swagger","swagger-ui","swaggerui","@swagger","@swagger-ui","@swaggerui","react-intersection-observer","react-intersection"
+
+# $npmFiles = "react-intersection-observer","react-intersection","react", "react-", "react-chart", "react-chartjs", "react-chartjs-2", "react-form", "react-hook", "react-hook-form", "react-use", "react-virtual", "react-window", "@react"
 
 # Neat little addition to searching over all the alphabet
 # $(65..90).foreach({npm search "@mapbox/$([char]$_)"})
@@ -113,9 +115,9 @@ while ($npmFilesArray.count -gt 0) {
 
 # cd node_modules
 # # cd C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\ToMove\\node\\node_modules\\
-# $directory=$(pwd)
-$folders = gci -Recurse -Directory
-$folders.foreach({ cd "$_" && npm i --legacy-peer-deps && npm audit fix --force })
+# # $directory=$(pwd)
+# $folders = gci -Recurse -Directory
+# $folders.foreach({ cd "$_" && npm i --legacy-peer-deps && npm audit fix --force })
 
 # # HISTORY
 
@@ -157,31 +159,31 @@ $folders.foreach({ cd "$_" && npm i --legacy-peer-deps && npm audit fix --force 
 # $(gci -recurse "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\ToMove\\" -Filter "*.tgz").foreach({Move-Item $_ -destination "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\ToMove\\node\\"})
 
 
-# DeDupe
-$moved = gci "*.tgz" -Recurse -Path "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\Moved\\Node\\"
-$toMove = gci -recurse "*.tgz" -Path "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\ToMove\\Node\\"
-# mkdir C:\Users\$env:USERNAME\Downloads\Staging
-$staging = "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\Staging\\Node\\"
-$count = 0
-$leavingCount = 0
-$toMove.foreach({ if ($_.name -notin $moved.Name) { $count++ } })
-$toMove.foreach({ if ($_.name -in $moved.Name) { $leavingCount++ } })
-write-host "Going to move $count packages"
-write-host "$leavingCount duplicate packages"
+# # DeDupe
+# $moved = gci "*.tgz" -Recurse -Path "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\Moved\\Node\\"
+# $toMove = gci -recurse "*.tgz" -Path "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\ToMove\\Node\\"
+# # mkdir C:\Users\$env:USERNAME\Downloads\Staging
+# $staging = "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\Staging\\Node\\"
+# $count = 0
+# $leavingCount = 0
+# $toMove.foreach({ if ($_.name -notin $moved.Name) { $count++ } })
+# $toMove.foreach({ if ($_.name -in $moved.Name) { $leavingCount++ } })
+# write-host "Going to move $count packages"
+# write-host "$leavingCount duplicate packages"
 
-# Move Files
-$toMove.foreach({ if ($_.name -notin $moved.Name) { move-item $_ $staging } })
+# # Move Files
+# $toMove.foreach({ if ($_.name -notin $moved.Name) { move-item $_ $staging } })
 
-# Hashing Files
-$Directory = "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\Staging\\node"
-get-filehash "$Directory\\*" | format-list | Out-File "$Directory\\..\\PRE_DTA_HASH_$(Get-Date -Format "yyyyMMdd")_node.txt"
+# # Hashing Files
+# $Directory = "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\Staging\\node"
+# get-filehash "$Directory\\*" | format-list | Out-File "$Directory\\..\\PRE_DTA_HASH_$(Get-Date -Format "yyyyMMdd")_node.txt"
 
 
-# $Directory = "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\Staging\\Software"
-# $toHash = gci -recurse -Path $Directory
-# $toHash.foreach({Get-FileHash $_ | format-list | Out-File -Append "$Directory\\..\\PRE_DTA_HASH_$(Get-Date -Format "yyyyMMdd")_Software.txt"})
+# # $Directory = "C:\\Users\\$env:USERNAME\\Downloads\\Transfer\\Staging\\Software"
+# # $toHash = gci -recurse -Path $Directory
+# # $toHash.foreach({Get-FileHash $_ | format-list | Out-File -Append "$Directory\\..\\PRE_DTA_HASH_$(Get-Date -Format "yyyyMMdd")_Software.txt"})
 
-# ------------------------------------------------------------
+# # ------------------------------------------------------------
 
-yarn config set registry http://10.245.42.25:4873/
-yarn config set unsafeHTTPWhiteList 10.245.42.25
+# yarn config set registry http://10.245.42.25:4873/
+# yarn config set unsafeHTTPWhiteList 10.245.42.25
