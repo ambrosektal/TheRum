637c377690f467baeb0cc8f42e8482fecfe2b800
Ambrose KTal <joe.spann@gmail.com>
2022-07-08 09:52:24 -0400

    lots of additions

--- vscsyncExtensionExpansion.ps1
+++ vscsyncExtensionExpansion.ps1
@@ -5,16 +5,27 @@ foreach($directory in $directories){
 	# cd $directory
 	$files = $(gc $directory\latest.json | convertfrom-json)
 	
-	if (!$(Test-Path $directory\$($_.version)\$($_.targetPlatform))) {
+	# if (!$(Test-Path $directory\$($_.version)\$($_.targetPlatform))) {
 		($files.versions).foreach({$(mkdir $directory\$($_.version)\$($_.targetPlatform) ) })	
 		($files.versions).foreach({$(cd $directory\$($_.version)\$($_.targetPlatform) && ($_.files.source).foreach({Invoke-WebRequest $_ -OutFile $_.Split("/")[-1]  }) ) })
-	} else {
-		Write-Host "Newest version of $directory\$($_.version)\$($_.targetPlatform) already exists"
-	}
-	
+	# } else {
+		# Write-Host "Newest version of $directory\$($_.version)\$($_.targetPlatform) already exists"
+	# }
 }
+
+
 # Remove extensions with under X installs
 # (gc latest.json | convertfrom-json).statistics.GetValue(0).value
-(gci -Recurse -Filter "latest.json" | where-object {(gc $_ | convertfrom-json).statistics.GetValue(0).value -lt 10000 }).Directory 
+# (gci -Recurse -Filter "latest.json" | where-object {(gc $_ | convertfrom-json).statistics.GetValue(0).value -lt 10000 }).Directory | rm -r -force
+(((gci -Recurse -Filter "latest.json" | where-object {(gc $_ | convertfrom-json).statistics.GetValue(0).value -lt 5000 }).Directory) | Where-Object {($_.Name -notlike "ms-*") -and ($_.Name -notlike "*svelte*")}).count
+# (((gci -Recurse -Filter "latest.json" | where-object {(gc $_ | convertfrom-json).statistics.GetValue(0).value -lt 5000 }).Directory) | Where-Object {($_.Name -notlike "ms-*") -and ($_.Name -notlike "*svelte*")}) | rm -r -Force
 
 
+# clean up specified.json
+$currentNames = <"paste in list from json">
+$removeNames = (((gci -Recurse -Filter "latest.json" | where-object {(gc $_ | convertfrom-json).statistics.GetValue(0).value -lt 5000 }).Directory) | Where-Object {($_.Name -notlike "ms-*") -and ($_.Name -notlike "*svelte*")}).name
+($currentNames | Where-Object {$_ -notin $removeNames})
+# Paste the output into the json
+
+# Remove the folders from the cache, based on number of downloads.
+(((gci -Recurse -Filter "latest.json" | where-object {(gc $_ | convertfrom-json).statistics.GetValue(0).value -lt 5000 }).Directory) | Where-Object {($_.Name -notlike "ms-*") -and ($_.Name -notlike "*svelte*")}) | rm -r -Force
